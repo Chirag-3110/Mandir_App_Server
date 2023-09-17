f3576072fd72fbdd19816fc394653d3ebad45b52
Rohit Saini <rohitpsaini005@gmail.com>
2023-09-17 23:56:43 +0530

    env

--- src/Config/DBConfig.ts
+++ src/Config/DBConfig.ts
@@ -1,20 +1,40 @@
 var mysql = require('mysql'); 
 
-export const  connection = mysql.createConnection({
-    host: process.env.HOST,
-    user: "root",
-    password: '',
-    database:"jainMandir"
-  });
-const configMongoDB = async (host:string,user:string,password:string) => {
+
+const options = {
+  host:  process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DBNAME
+} 
+
+export const  connection = mysql.createConnection(options);
+const configMongoDB = async () => {
+  
+  console.log(options,"options");
+  
     connection.connect(function(err,result) {
         if (err) throw err;
-        console.log("Connected!");
-        
-        connection.query("CREATE DATABASE jainMandir",function (err, result) {
+        connection.query(`CREATE DATABASE ${process.env.DBNAME}`,function (err, result) {
             if (err){
               console.log("Database connected");
             };
+            // const userTable = `CREATE TABLE IF NOT EXISTS user (
+            //   id INT AUTO_INCREMENT PRIMARY,
+            //   name TEXT,
+            //   email TEXT,
+            //   password TEXT,
+            //   created_at TEXT
+            // )`
+
+            // connection.query(userTable,(err,result)=>{
+            //   if(err){
+            //     console.log(err,"userTable");
+                
+            //   }
+            //   console.log(result);
+              
+            // })
             console.log("Database created");
           });
         
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,24 @@
 import express from 'express';
-// import authRoute from './Routes/Auth';
 import AdminAuthRoute from './AdminApi/Auth';
 import configMongoDB from './Config/DBConfig'
-const app = express();
 import config from 'dotenv'
 import UserController from './AdminApi/User';
+
+const app = express();
+
 config.config()
+
 const port = process.env.PORT || 8000;
+
 app.use(express.json())
+
 app.use(express.urlencoded({ extended: true }))
+
 app.listen(port, async () => {
-    await configMongoDB(process.env.HOST,process.env.USER,process.env.PASSWORD)
-    console.log(`listening at http://localhost:${port}`);
+    await configMongoDB()
 });
+
 app.use(AdminAuthRoute)
+
 app.use(UserController)
 // app.use(NewsRouter)
