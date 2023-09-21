e645af58ea7dff9a785adc947f6ee35296d3a748
Rohit Saini <rohitpsaini005@gmail.com>
2023-09-22 00:49:47 +0530

    users

--- src/AdminApi/User.ts
+++ src/AdminApi/User.ts
@@ -9,42 +9,42 @@ const upload = multer({ storage: storage }).single('file'); // 'file' should mat
 
 
 UserController.get("/get-users", (req, res) => {
-    
-   const isVerified = verifyToken(req)
-   console.log(isVerified);
-   
-   if(isVerified===true){
-    const page = 1;
-    const pageSize = 10;
 
-    const offset = (page - 1) * pageSize;
+    const isVerified = verifyToken(req)
+    console.log(isVerified);
+
+    if (isVerified === true) {
+        const page = 1;
+        const pageSize = 10;
+
+        const offset = (page - 1) * pageSize;
 
 
-    const query = `SELECT * FROM users LIMIT ? OFFSET ?`;
-    const values = [pageSize, offset];
+        const query = `SELECT * FROM users LIMIT ? OFFSET ?`;
+        const values = [pageSize, offset];
 
-    connection.query(query, values, (err, result) => {
-        if (err) {
+        connection.query(query, values, (err, result) => {
+            if (err) {
+                res.send({
+                    status: false,
+                    message: "Something went wrong",
+                    data: err
+                })
+            }
+
             res.send({
-                status: false,
-                message: "Something went wrong",
-                data: err
+                status: true,
+                message: "Users get successully",
+                data: result
             })
-        }
-
-        res.send({
-            status: true,
-            message: "Users get successully",
-            data: result
         })
-    })
-   }else{
-    res.send({
-        status: 401,
-        message: "Unauthenticated",
-        data: null
-    })
-   }
+    } else {
+        res.send({
+            status: 401,
+            message: "Unauthenticated",
+            data: null
+        })
+    }
 
 })
 
@@ -77,82 +77,82 @@ UserController.post("/get-file", upload, (req, res) => {
 
 UserController.post("/add-user", (req, res) => {
     const isVerified = verifyToken(req)
-   if(isVerified===true){
-    let request = req.body;
-    console.log(request, "body");
+    if (isVerified === true) {
+        let request = req.body;
+        console.log(request, "body");
 
 
 
-    if(request){
-        const getUser = "SELECT * FROM users WHERE phone = ? OR email = ?";
+        if (request) {
+            const getUser = "SELECT * FROM users WHERE phone = ? OR email = ?";
 
-    connection.query(getUser, [request.phone,,request.email], async (err, result) => {
-        if (err) {
-            res.send({
-                status: 503,
-                message: "internal server error",
-                data: null
-            })
-        }
-        let existUser = result[0];
-        if (existUser) {
-            res.send({
-                status: 404,
-                message: "phone or email already exist",
-                data: null
-            })
-
-        } else {
-            const setUser = "INSERT INTO users SET ?";
-            request.created_at = new Date().toUTCString()
-            connection.query(setUser, request, async (err, result) => {
-                console.log(err,"error is");
-                if (err) res.send({
-                    status: 500,
-                    message: "Internal server error",
-                    data: null
-                })
-
-                const query = "SELECT * FROM users WHERE phone = ?";
-             connection.query(query,[request.phone],async (err, result)=>{
+            connection.query(getUser, [request.phone, , request.email], async (err, result) => {
                 if (err) {
-                    console.log(err);
-                    
                     res.send({
-                        status: 500,
-                        message: "Internal server error",
+                        status: 503,
+                        message: "internal server error",
                         data: null
                     })
                 }
-            
-                
-                res.send({
-                    status: 201,
-                    message: "user created",
-                    data: result[0]
-                })
-             });
-                
-            })
+                let existUser = result[0];
+                if (existUser) {
+                    res.send({
+                        status: 404,
+                        message: "phone or email already exist",
+                        data: null
+                    })
 
+                } else {
+                    const setUser = "INSERT INTO users SET ?";
+                    request.created_at = new Date().toUTCString()
+                    connection.query(setUser, request, async (err, result) => {
+                        console.log(err, "error is");
+                        if (err) res.send({
+                            status: 500,
+                            message: "Internal server error",
+                            data: null
+                        })
+
+                        const query = "SELECT id,full_name,email,phone,gotra,address,occupation,age,gender,postal_address FROM users WHERE phone = ?";
+                        connection.query(query, [request.phone], async (err, result) => {
+                            if (err) {
+                                console.log(err);
+
+                                res.send({
+                                    status: 500,
+                                    message: "Internal server error",
+                                    data: null
+                                })
+                            }
+
+
+                            res.send({
+                                status: 201,
+                                message: "user created",
+                                data: result[0]
+                            })
+                        });
+
+                    })
+
+                }
+
+            })
+        } else {
+            res.send({
+                status: 404,
+                message: "request is empty",
+                data: null
+            })
         }
 
-    })
-    }else{
+    } else {
         res.send({
-            status: 404,
-            message: "request is empty",
+            status: 401,
+            message: "Unauthenticated",
             data: null
         })
     }
-
-   }else{
-    res.send({
-        status: 401,
-        message: "Unauthenticated",
-        data: null
-    })
-   }
 })
 
 
