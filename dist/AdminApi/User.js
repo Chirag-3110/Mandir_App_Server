d5b9571b3f97ae267ecfcfa3fe6248f6938d6876
Rohit Saini <rohitpsaini005@gmail.com>
2023-09-22 00:51:02 +0530

    users

--- dist/AdminApi/User.js
+++ dist/AdminApi/User.js
@@ -14,31 +14,43 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const DBConfig_1 = require("../Config/DBConfig");
+const HelperFunction_1 = require("../Middleware/HelperFunction");
 const XLSX = require('xlsx');
 const multer = require('multer');
 const UserController = express_1.default.Router();
 const storage = multer.memoryStorage(); // Store files in memory for parsing.
 const upload = multer({ storage: storage }).single('file'); // 'file' should match the 'name' attribute
 UserController.get("/get-users", (req, res) => {
-    const page = 1;
-    const pageSize = 10;
-    const offset = (page - 1) * pageSize;
-    const query = `SELECT * FROM users LIMIT ? OFFSET ?`;
-    const values = [pageSize, offset];
-    DBConfig_1.connection.query(query, values, (err, result) => {
-        if (err) {
+    const isVerified = (0, HelperFunction_1.verifyToken)(req);
+    console.log(isVerified);
+    if (isVerified === true) {
+        const page = 1;
+        const pageSize = 10;
+        const offset = (page - 1) * pageSize;
+        const query = `SELECT * FROM users LIMIT ? OFFSET ?`;
+        const values = [pageSize, offset];
+        DBConfig_1.connection.query(query, values, (err, result) => {
+            if (err) {
+                res.send({
+                    status: false,
+                    message: "Something went wrong",
+                    data: err
+                });
+            }
             res.send({
-                status: false,
-                message: "Something went wrong",
-                data: err
+                status: true,
+                message: "Users get successully",
+                data: result
             });
-        }
-        res.send({
-            status: true,
-            message: "Users get successully",
-            data: result
         });
-    });
+    }
+    else {
+        res.send({
+            status: 401,
+            message: "Unauthenticated",
+            data: null
+        });
+    }
 });
 UserController.post("/get-file", upload, (req, res) => {
     console.log(req.file);
@@ -63,61 +75,71 @@ UserController.post("/get-file", upload, (req, res) => {
     // })
 });
 UserController.post("/add-user", (req, res) => {
-    let request = req.body;
-    console.log(request, "body");
-    if (request) {
-        const getUser = "SELECT * FROM users WHERE phone = ? OR email = ?";
-        DBConfig_1.connection.query(getUser, [request.phone, , request.email], (err, result) => __awaiter(void 0, void 0, void 0, function* () {
-            if (err) {
-                res.send({
-                    status: 503,
-                    message: "internal server error",
-                    data: null
-                });
-            }
-            let existUser = result[0];
-            if (existUser) {
-                res.send({
-                    status: 404,
-                    message: "phone or email already exist",
-                    data: null
-                });
-            }
-            else {
-                const setUser = "INSERT INTO users SET ?";
-                request.created_at = new Date().toUTCString();
-                DBConfig_1.connection.query(setUser, request, (err, result) => __awaiter(void 0, void 0, void 0, function* () {
-                    console.log(err, "error is");
-                    if (err)
-                        res.send({
-                            status: 500,
-                            message: "Internal server error",
-                            data: null
-                        });
-                    const query = "SELECT * FROM users WHERE phone = ?";
-                    DBConfig_1.connection.query(query, [request.phone], (err, result) => __awaiter(void 0, void 0, void 0, function* () {
-                        if (err) {
-                            console.log(err);
+    const isVerified = (0, HelperFunction_1.verifyToken)(req);
+    if (isVerified === true) {
+        let request = req.body;
+        console.log(request, "body");
+        if (request) {
+            const getUser = "SELECT * FROM users WHERE phone = ? OR email = ?";
+            DBConfig_1.connection.query(getUser, [request.phone, , request.email], (err, result) => __awaiter(void 0, void 0, void 0, function* () {
+                if (err) {
+                    res.send({
+                        status: 503,
+                        message: "internal server error",
+                        data: null
+                    });
+                }
+                let existUser = result[0];
+                if (existUser) {
+                    res.send({
+                        status: 404,
+                        message: "phone or email already exist",
+                        data: null
+                    });
+                }
+                else {
+                    const setUser = "INSERT INTO users SET ?";
+                    request.created_at = new Date().toUTCString();
+                    DBConfig_1.connection.query(setUser, request, (err, result) => __awaiter(void 0, void 0, void 0, function* () {
+                        console.log(err, "error is");
+                        if (err)
                             res.send({
                                 status: 500,
                                 message: "Internal server error",
                                 data: null
                             });
-                        }
-                        res.send({
-                            status: 201,
-                            message: "user created",
-                            data: result[0]
-                        });
+                        const query = "SELECT id,full_name,email,phone,gotra,address,occupation,age,gender,postal_address FROM users WHERE phone = ?";
+                        DBConfig_1.connection.query(query, [request.phone], (err, result) => __awaiter(void 0, void 0, void 0, function* () {
+                            if (err) {
+                                console.log(err);
+                                res.send({
+                                    status: 500,
+                                    message: "Internal server error",
+                                    data: null
+                                });
+                            }
+                            res.send({
+                                status: 201,
+                                message: "user created",
+                                data: result[0]
+                            });
+                        }));
                     }));
-                }));
-            }
-        }));
+                }
+            }));
+        }
+        else {
+            res.send({
+                status: 404,
+                message: "request is empty",
+                data: null
+            });
+        }
     }
     else {
         res.send({
-            status: 404,
-            message: "request is empty",
+            status: 401,
+            message: "Unauthenticated",
             data: null
         });
     }
--- dist/Middleware/HelperFunction.js
+++ dist/Middleware/HelperFunction.js
@@ -1,6 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.addCutomRequest = exports.generateOTP = void 0;
+exports.verifyToken = exports.generateOTP = void 0;
+const env = require('dotenv');
+env.config();
+const jwt = require('jsonwebtoken');
 function generateOTP(length) {
     var digits = '0123456789';
     let OTP = '';
@@ -10,7 +13,25 @@ function generateOTP(length) {
     return OTP;
 }
 exports.generateOTP = generateOTP;
-function addCutomRequest(req) {
-}
-exports.addCutomRequest = addCutomRequest;
+const verifyToken = (req) => {
+    let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+    try {
+        const token = req.header(tokenHeaderKey);
+        console.log(req.headers.authorization.slice(7));
+        const verified = jwt.verify(req.headers.authorization.slice(7), jwtSecretKey);
+        if (verified) {
+            return true;
+        }
+        else {
+            // Access Denied
+            return false;
+        }
+    }
+    catch (error) {
+        // Access Denied
+        return false;
+    }
+};
+exports.verifyToken = verifyToken;
 //# sourceMappingURL=HelperFunction.js.map
