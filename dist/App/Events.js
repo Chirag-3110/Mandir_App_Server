078a8be87befdbeeb4488d558392e88c6106eeff
Rohit Saini <rohitpsaini005@gmail.com>
2023-10-02 00:08:34 +0530

    app apis

--- dist/App/Events.js
+++ dist/App/Events.js
@@ -1,1 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const HelperFunction_1 = require("../Middleware/HelperFunction");
+const express = require('express');
+const Events = express.Router;
+Events.get("/app/eventsList", (req, res) => {
+    const isVerified = (0, HelperFunction_1.verifyToken)(req);
+    if (isVerified === true) {
+    }
+    else {
+        res.send({
+            status: 401,
+            message: "Unauthenticated",
+            data: null
+        });
+    }
+});
+exports.default = Events;
 //# sourceMappingURL=Events.js.map
---
+++ src/App/Events.ts
@@ -0,0 +1,22 @@
+import { verifyToken } from "../Middleware/HelperFunction"
+
+const express = require('express')
+
+const Events = express.Router 
+
+Events.get("/app/eventsList",(req,res)=>{
+    const isVerified = verifyToken(req)
+
+    if(isVerified === true){
+
+    }else{
+        res.send({
+            status: 401,
+            message: "Unauthenticated",
+            data: null
+        })
+    }
+      
+})
+
+export default Events
