60f0584a76edccf3795ddf27de11d5dedca0ae36
Rohit Saini <rohitpsaini005@gmail.com>
2023-10-02 19:26:15 +0530

    app apis

---
+++ src/App/Users.ts
@@ -0,0 +1,28 @@
+import { connection } from "../Config/DBConfig";
+import { verifyToken } from "../Middleware/HelperFunction";
+
+const express = require('express')
+
+const AppProfile = express.Router() 
+
+AppProfile.get("/app/get-profile",(req,res)=>{
+    const isVerified = verifyToken(req);
+    if(isVerified === true){
+        connection.query('Select * FROM users WHERE id = ?',[req.query.id],(err,result)=>{
+            if (err) {
+                res.send({
+                    status: 500,
+                    message: "Internal server error",
+                    data: err
+                })
+            }
+            res.send({
+                status: 200,
+                message: "User fetched successfully",
+                data: result
+            });
+        })
+    }
+})
+
+export default AppProfile;
