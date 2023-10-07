c30988578f20e51cbd8e650ce0cf003831cd5f2e
Rohit Saini <rohitpsaini005@gmail.com>
2023-10-07 22:19:50 +0530

    edit event

--- dist/App/search_user.js
+++ dist/App/search_user.js
@@ -8,7 +8,7 @@ Searh.post("/user/search", (req, res) => {
     const isVerified = (0, HelperFunction_1.verifyToken)(req);
     if (isVerified === true) {
         const { query } = req.body;
-        const sql = `SELECT * FROM users WHERE full_name LIKE ?`;
+        const sql = `SELECT * FROM users WHERE full_name LIKE ? OR phone LIKE ? OR gotra LIKE ? OR occupation LIKE ?`;
         DBConfig_1.connection.query(sql, [`%${query}%`], (err, result) => {
             if (err) {
                 res.send({
--- src/App/search_user.ts
+++ src/App/search_user.ts
@@ -8,7 +8,7 @@ Searh.post("/user/search",(req,res)=>{
     const isVerified = verifyToken(req)
     if(isVerified === true){
         const { query } = req.body
-        const sql = `SELECT * FROM users WHERE full_name LIKE ?`;
+        const sql = `SELECT * FROM users WHERE full_name LIKE ? OR phone LIKE ? OR gotra LIKE ? OR occupation LIKE ?`;
 
         connection.query(sql,[`%${query}%`],(err,result)=>{
             if(err){
