f88a7fa42cca9278eca81bfd59fbea57cdeb2a56
Rohit Saini <ishanojha110@gmail.com>
2024-04-07 13:45:10 +0530

    cors

--- src/app.ts
+++ src/app.ts
@@ -27,15 +27,19 @@ var allowedOrigins = ['http://localhost:3000',
   
 app.use(express.json())
 
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', "true");
-    next();
-});
+const corsOpts = {
+  origin: '*',
 
-// app.use(cors())
+  methods: [
+    'GET',
+    'POST',
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+  ],
+};
+app.use(cors(corsOpts))
 
 app.use(express.urlencoded({ extended: true }))
 
